be7d2eceeb11f7c34e8b0be6570065714fb566d7
TwoCong <abc123456>
2015-12-11 11:46:08 +0800

    - 增加ID URL 两个索引域 - 对报错  重定向网页进行删除

---
+++ src/Controller/searchKey.java
@@ -0,0 +1,7 @@
+package Controller;
+
+/**
+ * Created by Two_Cong on 15/11/27.
+ */
+public class searchKey {
+}
---
+++ src/DAO/Result.java
@@ -0,0 +1,7 @@
+package DAO;
+
+/**
+ * Created by Two_Cong on 15/11/27.
+ */
+public class Result {
+}
